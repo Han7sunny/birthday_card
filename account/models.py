6d45f274cbb0db6dd670cfe3b4e67e55dcf0a4f6
Han7sunny <xoa1235@gmail.com>
2022-01-23 23:55:37 +0900

    html change

--- account/models.py
+++ account/models.py
@@ -1,3 +1,4 @@
+from optparse import Option
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
@@ -6,5 +7,5 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     # month = list(range(1,13))
     name = models.CharField(verbose_name='닉네임', max_length=20) # 생일 당사자
-    birthday = models.DateField(verbose_name='생년월일') # 20XXXXXX ,max_length=8
+    birthday = models.DateField(verbose_name='생년월일',max_length=8) # 20XXXXXX ,max_length=8
     # cards = models.Arr
